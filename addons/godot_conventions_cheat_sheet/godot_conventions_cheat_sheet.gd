50603f7a8af126efcea3c183a42703517d3f0014
DanilLut <131749970+DanilLut@users.noreply.github.com>
2024-12-20 16:28:52 +0200

    add changes.

--- addons/godot_conventions_cheat_sheet/godot_conventions_cheat_sheet.gd
+++ addons/godot_conventions_cheat_sheet/godot_conventions_cheat_sheet.gd
@@ -6,9 +6,9 @@ var dock: Control
 
 func _enter_tree() -> void:
 	dock = preload('res://addons/godot_conventions_cheat_sheet/dock.tscn').instantiate()
-	dock.name = 'Conventions Cheat Sheet'
+	dock.name = 'Conventions'
 
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_RIGHT_BL, dock)
+	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, dock)
 
 
 func _exit_tree() -> void:
